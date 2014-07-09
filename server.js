7b14d4c8dc8eff58a98be349ed532ca2af9af571
Pao <danielpowpao@gmail.com>
2014-07-08 17:22:29 -0700

    setup server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+var express        = require('express');
+var morgan         = require('morgan');
+var mongoose       = require('mongoose');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var app            = express();
+
+// configuration for db
+var db = require('./config/db');
+
+// set our port
+var port = process.env.PORT || 3000;
+// mongoose.connect(db.url);
+
+app.use(morgan('dev')); // logging to console for development use
+app.use(bodyParser()); // pull information from html in POST
+app.use(methodOverride()); // simulate DELETE and PUT
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+// configure routes
+require('./server/routes')(app); 
+
+// start app
+app.listen(port);
+console.log('Listening on port ' + port);
+exports = module.exports = app;
